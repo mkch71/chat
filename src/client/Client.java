80e9d2f20258c51fa55ba50d6a4d8f7ced1be8b6
Mike <mkch71@mail.ru>
2021-05-26 15:07:11 +0300

    Client ObjectInputStream

--- src/client/Client.java
+++ src/client/Client.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("192.168.70.70",8188);
+            Socket socket = new Socket("localhost",8188);
             System.out.println("Подключился");
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
